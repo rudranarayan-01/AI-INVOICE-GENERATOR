c0aa57b21d8e946928885000833e4a23dcd771c5
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-09-19 18:39:05 +0530

    Path Setup

--- frontend/invoice-generator/src/App.jsx
+++ frontend/invoice-generator/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
+import LandingPage from './pages/LandingPage/LAndingPage'
+import Signup from './pages/Auth/Signup'
+import Login from './pages/Auth/Login'
+
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage/>} />
+            <Route path='/signup' element={<Signup/>} />
+            <Route path='/login' element={<Login />} />
+
+            {/* Catch all routes  */}
+            <Route path='*' element={<Navigate to='/' replace />} />
+          </Routes>
+        </Router>
+        <Toaster toastOptions={{className:"" ,style:{fontSize:"13px",}, position: 'top-center'}} /> 
+    </div>
   )
 }
 
---
+++ frontend/invoice-generator/src/pages/Invoices/CreateInvoices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateInvoices = () => {
+  return (
+    <div>CreateInvoices</div>
+  )
+}
+
+export default CreateInvoices
