2d4c54926fc21940268a5e8a659a099a7d4a53fe
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-09-19 19:16:35 +0530

    Layout Sets Up

--- frontend/invoice-generator/src/components/auth/ProtectedRoute.jsx
+++ frontend/invoice-generator/src/components/auth/ProtectedRoute.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+const ProtectedRoute = ({ children }) => {
+    const isAuthenticated = true
+    const loading = false;
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return (
+        
+            <DashboardLayout>
+                {children ? children : <Outlet />}
+            </DashboardLayout>
+    )
 }
 
 export default ProtectedRoute
