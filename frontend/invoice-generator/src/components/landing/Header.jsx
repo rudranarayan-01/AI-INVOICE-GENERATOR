34c1edd9b6c21b32b36627cdba49242d4291a429
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-09-21 07:08:50 +0530

    Designing Header

--- frontend/invoice-generator/src/components/landing/Header.jsx
+++ frontend/invoice-generator/src/components/landing/Header.jsx
@@ -23,7 +23,34 @@ const Header = () => {
     }, []);
 
     return (
-        <div>Header</div>
+        <header className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-gray-100 ${isScrolled ? 'bg-white/95 backdrop-blur-sm shadow-lg' : 'bg-white/0'} `}>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex items-center justify-between h-16 lg:h-20'>
+                    <div className='flex items-center space-x-2'>
+                        <div className='w-8 h-8 flex items-center justify-center rounded-md bg-blue-900'>
+                            <FileText className='h-8 w-8 text-blue-600'/>
+                        </div>
+                        <span className=''>
+                            AI Invoice Generator
+                        </span>
+                    </div>
+                    <div className="">
+                        <a href="#features" className="">Features</a>
+                        <a href="#testimonial" className="">Testimonials</a>
+                        <a href="#faq" className="">FAQ</a>
+                        <div className="">
+                            <Link to="/login" className="">Login</Link>
+                            <Link to="/signup" className="">Sign Up</Link>
+                        </div>
+                        <div className="">
+                            <button onClick={()=>setIsMenuOpen(!isMenuOpen)} className=''>
+                                {isMenuOpen ? <X className='h-6 w-6'/> : <Menu className='h-6 w-6'/>}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
